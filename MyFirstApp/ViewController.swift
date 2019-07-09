d34b38c6a95e9e05cb96b26e823ccbd8df9a5caf
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-09 14:38:50 +0300

    first commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+    
+    imageView.image = UIImage(named: "metallica2")
+    
+    
+    }
+    
 }
 
